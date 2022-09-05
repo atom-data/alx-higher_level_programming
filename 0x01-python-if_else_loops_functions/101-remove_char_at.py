71886f7aa2d10a86d50c69301a7d2fe8ceb0ef60
atom-data <alatatomondi@gmail.com>
2022-09-05 09:30:48 -0700

    updated file

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 def remove_char_at(str, n):
     "Remove characters at position n"
     if n >= 0:
-        newstr=str[:n] + str[n +1:]
-        return newstr
+        newstring = str[:n] + str[n + 1:]
+        return newstring
     else:
         return str
