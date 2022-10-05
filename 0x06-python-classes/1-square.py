ee1afe66e481428652e04f5001796ef80aa8d67c
atom-data <alatatomondi@gmail.com>
2022-10-05 08:20:15 -0700

    Create a class square and initialize its size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define class Square"""
+
+
+class Square:
+    """Representation of a square
+
+    Attributes:
+    __size (int): The length of a side of a square
+    """
+    def __init__(self, size):
+        """Initializes the class square
+
+        Args:
+        size (int): The length of a side of a square
+        """
+        self.__size = size
---
+++ 0x06-python-classes/m1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    def __init__(self, size):
+        """Initializes the class square
+
+        Args:
+        size (int): The length of a side of a square
+
+        """
+        self.__size = size
