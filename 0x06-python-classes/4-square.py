d7c8f9c4c39fbbef29c9a9e090cb350e8de59e59
atom-data <alatatomondi@gmail.com>
2022-10-06 06:01:26 -0700

    create a property for size

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Define the class Square"""
+
+
+class Square:
+    """Representation of a square
+
+    Attributes:
+    __size (int): The length of a side of a square
+    """
+    def __init__(self, size=0):
+        """Initializes the class square
+
+        Args:
+        size (int): The length of a side of a square
+        """
+        self.size = size
+    def area(self):
+        """Compute the area of a square
+
+        Returns:
+        The area of a square
+        """
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        """getter of __size attribute
+
+        Returns:
+        The value of __size attribute
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Assign value to __size
+
+        Args:
+        value (int): The length of a side of a square
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
