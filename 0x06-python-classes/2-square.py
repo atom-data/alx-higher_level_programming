dce4145c86ee2eba92966f5bf2093a88ef793400
atom-data <alatatomondi@gmail.com>
2022-10-05 23:37:00 -0700

    Define a square with valid type and value

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define the class Square"""
+
+
+class Square:
+    """Representation of a square
+
+    Attributes:
+    __size (int): The length of a side of a square
+    """
+    def __init__(self, size=0):
+        """Initializes the class square
+
+        Args:
+        size (int): The length of a side of a square
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
