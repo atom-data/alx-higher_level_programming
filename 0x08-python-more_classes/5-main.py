8a9c35e1edec87d4ee1438fd9d1464775e724769
atom-data <alatatomondi@gmail.com>
2022-10-14 14:05:01 -0700

    defining del method for rectangle

--- 0x08-python-more_classes/5-main.py
+++ 0x08-python-more_classes/5-main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-Rectangle = __import__('6-rectangle').Rectangle
+Rectangle = __import__('5-rectangle').Rectangle
 
 my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter())
 
 del my_rectangle
 
 try:
-    print(my_rectangle)
+	print(my_rectangle)
 except Exception as e:
-    print("[{}] {}".format(e.__.__class__.name__, e)
+	print("[{}] {}".format(e.__class__.__name__, e))
