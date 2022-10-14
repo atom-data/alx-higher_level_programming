947cbca76626f2b17b4842f24ffe8367ce785d37
atom-data <alatatomondi@gmail.com>
2022-10-14 14:44:25 -0700

    Define a class public attribute for print symbol

--- 0x08-python-more_classes/7-main.py
+++ 0x08-python-more_classes/7-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 Rectangle = __import__('7-rectangle').Rectangle
 
-my_rectangle = Reectangle(8, 4)
+my_rectangle_1 = Rectangle(8, 4)
 print(my_rectangle_1)
 print("--")
 my_rectangle_1.print_symbol = "&"
