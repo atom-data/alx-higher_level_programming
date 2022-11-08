88de9ae179a13af80a729c8dd37ed40e399a8a8e
atom-data <alatatomondi@gmail.com>
2022-11-08 06:39:08 -0800

    Force an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Definition of class Base Geometry"""
+
+
+class BaseGeometry:
+    """Base Geometry"""
+
+    def area(self):
+        """Force an error"""
+        raise Exception("area() is not implemented")
