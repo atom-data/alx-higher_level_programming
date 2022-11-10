425619abf0c894a7af82d8dcb0f98583a0d26604
atom-data <alatatomondi@gmail.com>
2022-11-10 09:57:13 -0800

    rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Definition of class Base Geometry and Rectangle"""
+
+
+class BaseGeometry:
+    """Base Geometry"""
+
+    def area(self):
+        """Force an error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate value
+        Args:
+        name (string): Element of error message
+        value (int): number to be validated
+        """
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than zero")
+
+
+class Rectangle(BaseGeometry):
+    """Subclass of BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialize a rectangle
+        Args:
+        width (int): width of the rectangle
+        height (int): height of the rectangle
+        """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
