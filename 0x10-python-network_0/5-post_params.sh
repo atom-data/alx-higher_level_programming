dd3565a08b43939ed1c13d8e875b4d433fde3352
atom-data <alatatomondi@gmail.com>
2022-12-20 12:12:08 -0800

    take in a URL and send a POST request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a POST request and displays the body response
-curl -s "$1" -X POST -d "test@gmail.com&subject=I will always be here for PLD"
+# Send a POST request to User's URL w/2 vars & display the body of the response
+curl -sX POST --data "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
