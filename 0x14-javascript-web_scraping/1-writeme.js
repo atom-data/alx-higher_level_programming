0d88a364d9e0570ff5f5083946d14cd769a22766
atom-data <alatatomondi@gmail.com>
2023-01-17 17:46:48 +0000

    write a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,3 +1,15 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.writeFileSync(process.argv[2], process.argv[3]);
+
+let args = process.argv.slice(2);
+if (args[0] === undefined) {
+  console.log('No argument');
+} else if (args.length !== 2) {
+  process.exit();
+}
+
+let fs = require('fs');
+fs.writeFile(args[0], args[1], 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
