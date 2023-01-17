bec947f5ac15b3c8ed5f1da7f5c416832d7db836
atom-data <alatatomondi@gmail.com>
2023-01-17 17:41:30 +0000

    new readme file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,16 @@
-t fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, contents) {
-  if (contents === undefined) {
+#!/usr/bin/node
+let args = process.argv.slice(2);
+if (args[0] === undefined) {
+  console.log('No argument');
+} else if (args.length !== 1) {
+  process.exit();
+}
+
+let fs = require('fs');
+fs.readFile(args[0], 'utf8', function read (err, data) {
+  if (err) {
     console.log(err);
   } else {
-    console.log(contents);
+    console.log(data);
   }
 });
